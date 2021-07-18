3c9c69d718cb0ffa6bc203874b969090a98a29cb
Piyush Mehta <me@piyushmehta.com>
2021-07-18 22:43:03 +0530

    fixing

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import React from "react";
 import "./Header.css";
-import PersonIcon from '@material-ui/icons/Person';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
-    return (
-
-        <div className="header">
-        <h2>I am header</h2>
-
-        {/* <AccountCircleIcon /> */}
-        </div>
-    );
+  return (
+    <div className="header">
+      <h2>I am header</h2>
+      <AccountCircleIcon />
+    </div>
+  );
 }
 
 export default Header;
