c2cbaedeacf4923bada949bf70331c6f53f5e600
rakeshsangolli <rakesh.sangolli00@gmail.com>
2021-07-18 22:19:55 +0530

    Initial commit

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,21 @@
+import react from "react";
+import './App.css';
+import Header from "./Header";
+
+
+function App() {
+  return (
+    <div className="app">
+      
+
+    {/* Header */}
+    <Header />
+
+    {/* TinderCards */}
+    {/* SwipeButtons */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+function Header() {
+    return (
+
+        <div className="header">
+        <h2>I am header</h2>
+
+        {/* <AccountCircleIcon /> */}
+        </div>
+    );
+}
+
+export default Header;
